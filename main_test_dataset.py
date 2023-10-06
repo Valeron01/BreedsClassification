8a2e6efad12c9cdf50425c3f673e35771aa418d6
Valeron01 <electronpochta3011@mail.ru>
2023-10-07 01:06:35 +0300

    Added visualization of train dataset

---
+++ main_test_dataset.py
@@ -0,0 +1,23 @@
+from argparse import ArgumentParser
+
+import cv2
+
+import augmentations
+from datasets.dog_breed_identification_dataset import DogBreedIdentificationDataset
+
+
+def main():
+    parser = ArgumentParser()
+    parser.add_argument("--dataset_path", required=True)
+    parsed_args = parser.parse_args()
+
+    dataset = DogBreedIdentificationDataset(parsed_args.dataset_path, image_transform=augmentations.get_augmentations())
+
+    for image, class_index in dataset:
+        print(dataset.all_classes[class_index])
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
+
+
+if __name__ == '__main__':
+    main()
